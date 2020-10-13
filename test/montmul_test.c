57a76d712aa936ead5719d8c2ebfe1ccf7d765b2
piggypiggy <1091267149@qq.com>
2020-10-13 20:39:07 +0800

    add fp256_from_mont test

--- test/montmul_test.c
+++ test/montmul_test.c
@@ -25,7 +25,7 @@
 void* fp256_mont_mul_test(void *p)
 {
     int64_t i, N;
-    fp256 r, tr1, tr2, a, b, A, B, odd, mod, k0;
+    fp256 r, tr1, tr2, a, b, ta, tb, A, B, odd, mod, k0;
     size_t al, bl, ol, w;
     mont_ctx mctx;
     TEST_THREAD_DATA *td;
@@ -47,6 +47,9 @@ void* fp256_mont_mul_test(void *p)
             w = ol;
         } while (fp256_is_odd(&odd) != 1);
         fp256_mont_ctx_init(&mctx, w, &odd);
+        /* a, b should be less than modulo */
+        fp256_mod(&a, &a, &odd);
+        fp256_mod(&b, &b, &odd);
 
         /* check k0 * N = -1 mod 2^64 */
         fp256_set_limb(&k0, mctx.k0);
@@ -62,15 +65,31 @@ void* fp256_mont_mul_test(void *p)
 
         /* A = aR mod N */
         fp256_to_mont(&A, &a, &mctx);
+        /* ta = a */
+        fp256_from_mont(&ta, &A, &mctx);
         /* B = bR mod N */
         fp256_to_mont(&B, &b, &mctx);
+        /* ta = a */
+        fp256_from_mont(&tb, &B, &mctx);
         /* r = abR mod N */
         fp256_mont_mul(&r, &A, &B, &mctx);
-        // fp256_mont_mul(&r, &a, &b, &mctx);
         /* tr1 = Ab mod N = abR mod N */
         fp256_mod_mul(&tr1, &A, &b, &mctx.N);
         /* tr2 = aB mod N = abR mod N */
         fp256_mod_mul(&tr2, &a, &B, &mctx.N);
+
+        /* check a = ta, b = tb */
+        if (fp256_cmp(&a, &ta) != 0 || fp256_cmp(&b, &tb) != 0) {
+            printf("fp256_from_mont %" PRId64 " failed\n", i + 1);
+            test_fp256_print_mont_ctx("mont ctx:\n", &mctx);
+            test_fp256_print_hex("a   = ", &a);
+            test_fp256_print_hex("A   = ", &A);
+            test_fp256_print_hex("ta  = ", &ta);
+            test_fp256_print_hex("b   = ", &b);
+            test_fp256_print_hex("B   = ", &B);
+            test_fp256_print_hex("tb  = ", &tb);
+            goto end;
+        }
 
         /* check r = tr1 = tr2 */
         if (fp256_cmp(&r, &tr1) != 0 || fp256_cmp(&r, &tr2) != 0) {
