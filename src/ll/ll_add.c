c40553f994038158d8dd422e3199fc2308d1fe52
piggypiggy <1091267149@qq.com>
2020-10-21 20:20:01 +0800

    add C implementation of ll_add_limb(s) and  ll_sub_limb(s)

--- src/ll/ll_add.c
+++ src/ll/ll_add.c
@@ -1,17 +1,47 @@
 #include <fp256/fp256_ll.h>
 #include "ll_local.h"
 
-#ifndef USE_ASM
-u64 ll_add_limb(u64 *rd, u64 *ad, u64 b, size_t al)
+// #ifndef USE_ASM
+u64 ll_add_limb(u64 *rd, const u64 *ad, u64 b, size_t al)
 {
-    return 0;
+    u64 carry, t1, t2;
+    size_t i;
+
+    carry = 0;
+    for (i = 0; i < al; i++) {
+        t1 = ad[0] + carry;
+        ad++;
+        carry = (t1 < carry);
+        t2 = b + t1;
+        rd[0] = t2;
+        carry += (t2 < t1);
+        b = 0;
+        rd++;
+    }
+
+    return carry;
 }
 
 u64 ll_add_limbs(u64 *rd, const u64 *ad, const u64 *bd, size_t l)
 {
-    return 0;
+    u64 carry, t1, t2;
+    size_t i;
+
+    carry = 0;
+    for (i = 0; i < l; i++) {
+        t1 = ad[0] + carry;
+        ad++;
+        carry = (t1 < carry);
+        t2 = bd[0] + t1;
+        bd++;
+        rd[0] = t2;
+        carry += (t2 < t1);
+        rd++;
+    }
+
+    return carry;
 }
-#endif
+// #endif
 
 size_t ll_add(u64 *rd, const u64 *ad, const u64 *bd, size_t al, size_t bl)
 {
@@ -38,17 +68,46 @@ size_t ll_add(u64 *rd, const u64 *ad, const u64 *bd, size_t al, size_t bl)
     return l;
 }
 
-#ifndef USE_ASM
-u64 ll_sub_limb(u64 *rd, u64 *ad, u64 b, size_t al)
+// #ifndef USE_ASM
+u64 ll_sub_limb(u64 *rd, const u64 *ad, u64 b, size_t al)
 {
-    return 0;
+    u64 borrow, t1;
+    size_t i;
+
+    borrow = 0;
+    for (i = 0; i < al; i++) {
+        t1 = ad[0];
+        ad++;
+        rd[0] = t1 - b - borrow;
+        if (t1 != b)
+            borrow = (t1 < b);
+        rd++;
+        b = 0;
+    }
+
+    return borrow;
 }
 
 u64 ll_sub_limbs(u64 *rd, const u64 *ad, const u64 *bd, size_t l)
 {
-    return 0;
+    u64 borrow, t1, t2;
+    size_t i;
+
+    borrow = 0;
+    for (i = 0; i < l; i++) {
+        t1 = ad[0];
+        t2 = bd[0];
+        ad++;
+        bd++;
+        rd[0] = t1 - t2 - borrow;
+        if (t1 != t2)
+            borrow = (t1 < t2);
+        rd++;
+    }
+
+    return borrow;
 }
-#endif
+// #endif
 
 size_t ll_sub(u64 *rd, const u64 *ad, const u64 *bd, size_t al, size_t bl)
 {
