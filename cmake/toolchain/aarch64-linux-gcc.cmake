475e0788c1152e788fa4364d9b9806e389a06493
piggypiggy <1091267149@qq.com>
2020-11-20 16:31:22 +0800

    root path

--- cmake/toolchain/aarch64-linux-gcc.cmake
+++ cmake/toolchain/aarch64-linux-gcc.cmake
@@ -5,3 +5,5 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_C_COMPILER ${toolchain_path}/aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER ${toolchain_path}/aarch64-linux-gnu-g++)
+
+set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
