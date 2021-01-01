9faf3e014afd407c3b88d22a4a43b12fe8a1d708
piggypiggy <1091267149@qq.com>
2021-01-01 23:31:59 +0800

    2021

--- test/u256shift_test.c
+++ test/u256shift_test.c
@@ -1,6 +1,6 @@
 /******************************************************************************
  *                                                                            *
- * Copyright 2020 Meng-Shan Jiang                                             *
+ * Copyright 2020-2021 Meng-Shan Jiang                                        *
  *                                                                            *
  * Licensed under the Apache License, Version 2.0 (the "License");            *
  * you may not use this file except in compliance with the License.           *
