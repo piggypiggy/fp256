beaebb46ca438fd36f1018c4695fe57b0b4e2d6f
piggypiggy <1091267149@qq.com>
2021-02-13 17:46:52 +0800

    add benchmark code for riscv

--- bench/bench.h
+++ bench/bench.h
@@ -91,6 +91,8 @@ static inline unsigned __int64 gettime_i64()
 #define TICKS() (end_ticks - start_ticks)
 #define USEC() ((end_time - start_time) / 10)
 
+/* ------------------------------------------------------------------------- */
+
 #elif defined(__GNUC__) && defined(ARCH_AARCH64)
 #include "sys/time.h"
 #define BENCH_VARS               \
@@ -124,6 +126,42 @@ static inline int64_t gettime_i64()
 
 #define TICKS() (end_ticks - start_ticks)
 #define USEC() (end_time - start_time)
+
+/* ------------------------------------------------------------------------- */
+
+#elif defined(__GNUC__) && defined(ARCH_RISCV64)
+#include "sys/time.h"
+#define BENCH_VARS               \
+    u64 start_ticks, end_ticks;      \
+    int64_t start_time, end_time                                                              
+
+#define COUNTER_START()          \
+  __asm__ volatile               \
+    ("rdcycle %0          \n"    \
+     : "=r" (start_ticks))
+
+#define COUNTER_STOP()           \
+  __asm__ volatile               \
+    ("rdcycle %0          \n"    \
+     : "=r" (end_ticks))
+
+static inline int64_t gettime_i64()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000000UL + tv.tv_usec;
+}
+
+#define TIMER_START()            \
+    start_time = gettime_i64()
+
+#define TIMER_STOP()             \
+    end_time = gettime_i64()
+
+#define TICKS() (end_ticks - start_ticks)
+#define USEC() (end_time - start_time)
+
+/* ------------------------------------------------------------------------- */
 #else
 
 #error "timer not implemented"
