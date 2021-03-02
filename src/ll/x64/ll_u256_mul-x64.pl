f0d1924aa93cccded9365d5b06aeb7f3d8e1374e
piggypiggy <1091267149@qq.com>
2021-03-02 21:28:04 +0800

    u256_mullo, u256_sqrlo x64 impl

--- src/ll/x64/ll_u256_mul-x64.pl
+++ src/ll/x64/ll_u256_mul-x64.pl
@@ -44,211 +44,6 @@ my ($acc0,$acc1,$acc2,$acc3)=map("%r$_",(8..11));
 my ($t0,$t1,$t2,$t3,$t4)=("%rcx","%rbp","%rbx","%rsi","%r12");
 $code.=<<___;
 
-# void ll_u256_mul_limbs(u64 r[8], u64 a[4], u64 b[4], int al, int bl);
-.globl	ll_u256_mul_limbs
-.type	ll_u256_mul_limbs,\@function,5
-.align	32
-ll_u256_mul_limbs:
-    push	%rbp
-    push	%rbx
-    push	%r12
-    push	%r13
-    push	%r14
-    push	%r15
-
-    mov %rcx, $al
-    mov %r8, $bl
-    mov $b_org, $b_ptr
-
-    mov 0*8($a_ptr), $t0
-     xor $acc2, $acc2
-    mov 1*8($a_ptr), $t1
-    mov 2*8($a_ptr), $t2
-    mov 3*8($a_ptr), $t3
-
-#mul_b0:
-    mov 0*8($b_ptr), $t4
-    mov $t4, %rax
-    mulq $t0                # a[0]*b[0]
-     xor $acc3, $acc3
-    mov %rax, $acc0
-    mov %rdx, $acc1
-    mov $acc0, 0*8($r_ptr)
-    xor $acc0, $acc0
-    cmp \$1, $al
-    jbe .mul_b1
-    mov $t4, %rax
-    mulq $t1                # a[1]*b[0]
-    add %rax, $acc1
-    adc \$0, %rdx
-    mov %rdx, $acc2
-    cmp \$2, $al
-    jbe .mul_b1
-    mov $t4, %rax
-    mulq $t2                # a[2]*b[0]
-    add %rax, $acc2
-    adc \$0, %rdx
-    mov %rdx, $acc3
-    cmp \$3, $al
-    jbe .mul_b1
-    mov $t4, %rax
-    mulq $t3                # a[3]*b[0]
-    add %rax, $acc3
-    mov %rdx, $acc0
-    adc \$0, $acc0
-
-.mul_b1:
-    cmp \$1, $bl
-    jbe .done_mul1
-    mov 1*8($b_ptr), $t4
-    mov $t4, %rax
-    mulq $t0                # a[0]*b[1]
-    add %rax, $acc1
-    adc %rdx, $acc2
-    adc \$0, $acc3 
-    mov $acc1, 1*8($r_ptr)
-    adc \$0, $acc0
-    mov \$0, $acc1
-    # xor $acc1, $acc1
-    adc \$0, $acc1
-    cmp \$1, $al
-    jbe .mul_b2
-    mov $t4, %rax
-    mulq $t1                # a[1]*b[1]
-    add %rax, $acc2
-    adc %rdx, $acc3
-    adc \$0, $acc0
-    adc \$0, $acc1 
-    cmp \$2, $al
-    jbe .mul_b2
-    mov $t4, %rax
-    mulq $t2                # a[2]*b[1]
-    add %rax, $acc3
-    adc %rdx, $acc0
-    adc \$0, $acc1
-    cmp \$3, $al
-    jbe .mul_b2
-    mov $t4, %rax
-    mulq $t3                # a[3]*b[1]
-    add %rax, $acc0
-    adc %rdx, $acc1
-
-.mul_b2:
-    cmp \$2, $bl
-    jbe .done_mul2
-    mov 2*8($b_ptr), $t4
-    mov $t4, %rax
-    mulq $t0                # a[0]*b[2]
-    add %rax, $acc2
-    adc %rdx, $acc3
-    adc \$0, $acc0
-    mov $acc2, 2*8($r_ptr)
-    adc \$0, $acc1
-    # xor $acc2, $acc2
-    mov \$0, $acc2
-    adc \$0, $acc2
-    cmp \$1, $al
-    jbe .mul_b3
-    mov $t4, %rax
-    mulq $t1                # a[1]*b[2]
-    add %rax, $acc3
-    adc %rdx, $acc0
-    adc \$0, $acc1
-    adc \$0, $acc2
-    cmp \$2, $al
-    jbe .mul_b3
-    mov $t4, %rax
-    mulq $t2                # a[2]*b[2]
-    add %rax, $acc0
-    adc %rdx, $acc1
-    adc \$0, $acc2
-    cmp \$3, $al
-    jbe .mul_b3
-    mov $t4, %rax
-    mulq $t3                # a[3]*b[2]
-    add %rax, $acc1
-    adc %rdx, $acc2
-
-.mul_b3:
-    cmp \$3, $bl
-    jbe .done_mul3
-    mov 3*8($b_ptr), $t4
-    mov $t4, %rax
-    mulq $t0                # a[0]*b[3]
-    add %rax, $acc3
-    adc %rdx, $acc0
-    adc \$0, $acc1
-    mov $acc3, 3*8($r_ptr)
-    adc \$0, $acc2
-    # xor $acc3, $acc3
-    mov \$0, $acc3
-    adc \$0, $acc3
-    cmp \$1, $al
-    jbe .done_mul4
-    mov $t4, %rax
-    mulq $t1                # a[1]*b[3]
-    add %rax, $acc0
-    adc %rdx, $acc1
-    adc \$0, $acc2
-    adc \$0, $acc3
-    cmp \$2, $al
-    jbe .done_mul4
-    mov $t4, %rax
-    mulq $t2                # a[2]*b[3]
-    add %rax, $acc1
-    adc %rdx, $acc2
-    adc \$0, $acc3
-    cmp \$3, $al
-    jbe .done_mul4
-    mov $t4, %rax
-    mulq $t3                # a[3]*b[3]
-    add %rax, $acc2
-    adc %rdx, $acc3
-    jmp .done_mul4
-
-.done_mul1:
-    mov $acc1, 1*8($r_ptr)
-    mov $acc2, 2*8($r_ptr)
-    mov $acc3, 3*8($r_ptr)
-    mov $acc0, 4*8($r_ptr)
-    movq \$0, 5*8($r_ptr)
-    movq \$0, 6*8($r_ptr)
-    movq \$0, 7*8($r_ptr)
-    jmp .done_mul
-
-.done_mul2:
-    mov $acc2, 2*8($r_ptr)
-    mov $acc3, 3*8($r_ptr)
-    mov $acc0, 4*8($r_ptr)
-    mov $acc1, 5*8($r_ptr)
-    movq \$0, 6*8($r_ptr)
-    movq \$0, 7*8($r_ptr)
-    jmp .done_mul
-
-.done_mul3:
-    mov $acc3, 3*8($r_ptr)
-    mov $acc0, 4*8($r_ptr)
-    mov $acc1, 5*8($r_ptr)
-    mov $acc2, 6*8($r_ptr)
-    movq \$0, 7*8($r_ptr)
-    jmp .done_mul
-
-.done_mul4:
-    mov $acc0, 4*8($r_ptr)
-    mov $acc1, 5*8($r_ptr)
-    mov $acc2, 6*8($r_ptr)
-    mov $acc3, 7*8($r_ptr)
-
-.done_mul:
-    pop	%r15
-    pop	%r14
-    pop	%r13
-    pop	%r12
-    pop	%rbx
-    pop	%rbp
-    ret
-.size	ll_u256_mul_limbs,.-ll_u256_mul_limbs
-
 # void ll_u256_mul(u64 r[8], u64 a[4], u64 b[4]);
 .globl	ll_u256_mul
 .type	ll_u256_mul,\@function,3
@@ -393,6 +188,91 @@ ll_u256_mul:
     pop	%rbp
     ret
 .size	ll_u256_mul,.-ll_u256_mul
+
+
+# void ll_u256_mullo(u64 r[4], u64 a[4], u64 b[4]);
+.globl	ll_u256_mullo
+.type	ll_u256_mullo,\@function,3
+.align	32
+ll_u256_mullo:
+    push	%rbp
+    push	%rbx
+    push	%r12
+    push	%r13
+
+    mov $b_org, $b_ptr
+
+    mov 0*8($a_ptr), $t0
+    mov 1*8($a_ptr), $t1
+    mov 2*8($a_ptr), $t2
+    mov 3*8($a_ptr), $t3
+
+#mul_b0:
+    mov 0*8($b_ptr), $t4
+    mov $t4, %rax
+    mulq $t0                # a[0]*b[0]
+    mov %rax, $acc0
+    mov %rdx, $acc1
+     mov $t4, %rax
+    mov $acc0, 0*8($r_ptr)
+
+    mulq $t1                # a[1]*b[0]
+    add %rax, $acc1
+    mov %rdx, $acc2
+     mov $t4, %rax
+    adc \$0, $acc2
+
+    mulq $t2                # a[2]*b[0]
+    add %rax, $acc2
+    mov %rdx, $acc3
+     mov $t4, %rax
+    adc \$0, $acc3
+
+    mulq $t3                # a[3]*b[0]
+     mov 1*8($b_ptr), $t4
+    add %rax, $acc3
+
+#mul_b1:
+    mov $t4, %rax
+    mulq $t0                # a[0]*b[1]
+    add %rax, $acc1
+    adc %rdx, $acc2
+     mov $t4, %rax
+    adc \$0, $acc3 
+    mov $acc1, 1*8($r_ptr)
+
+    mulq $t1                # a[1]*b[1]
+    add %rax, $acc2
+     mov $t4, %rax
+    adc %rdx, $acc3
+
+    mulq $t2                # a[2]*b[1]
+    add %rax, $acc3
+     mov 2*8($b_ptr), $t4
+
+#mul_b2:
+    mov $t4, %rax
+    mulq $t0                # a[0]*b[2]
+    add %rax, $acc2
+     mov $t4, %rax
+    adc %rdx, $acc3
+    mov $acc2, 2*8($r_ptr)
+
+    mulq $t1                # a[1]*b[2]
+    add %rax, $acc3
+
+#mul_b3:
+    mov 3*8($b_ptr), %rax
+    mulq $t0                # a[0]*b[3]
+    add %rax, $acc3
+    mov $acc3, 3*8($r_ptr)
+
+    pop	%r13
+    pop	%r12
+    pop	%rbx
+    pop	%rbp
+    ret
+.size	ll_u256_mullo,.-ll_u256_mullo
 ___
 }
 
@@ -506,6 +386,70 @@ ll_u256_sqr:
     pop %rbp
     ret
 .size	ll_u256_sqr,.-ll_u256_sqr
+
+
+# void ll_u256_sqrlo(u64 r[4], u64 a[4]);
+.globl	ll_u256_sqrlo
+.type	ll_u256_sqrlo,\@function,2
+.align	32
+ll_u256_sqrlo:
+    push %rbp
+    push %rbx
+    push %r12
+
+    mov 8*0($a_ptr), %rax
+    mov 8*1($a_ptr), $acc6
+    mov 8*2($a_ptr), $acc7
+    mov 8*3($a_ptr), $acc0
+
+    mov %rax, $acc5
+    mulq $acc6          # a[1]*a[0]
+    mov %rax, $acc1
+    mov $acc7, %rax
+    mov %rdx, $acc2
+
+    mulq $acc5          # a[0]*a[2]
+    add %rax, $acc2
+    mov $acc0, %rax
+    adc \$0, %rdx
+    mov %rdx, $acc3
+
+    mulq $acc5          # a[0]*a[3]
+    add %rax, $acc3
+    mov $acc7, %rax
+
+    #################################
+    mulq $acc6          # a[1]*a[2]
+    mov \$0, $acc7
+    add %rax, $acc3
+
+    add $acc1, $acc1    # acc1:3<<1
+     mov $acc5, %rax
+    adc $acc2, $acc2
+    adc $acc3, $acc3
+
+    mulq %rax
+    mov %rax, $acc0
+    mov $acc6, %rax
+    add %rdx, $acc1
+     mov $acc0, 0*8($r_ptr)
+    adc \$0, $acc7      # carry
+
+    mulq %rax
+     mov $acc1, 1*8($r_ptr)
+    add %rax, $acc2
+    adc %rdx, $acc3
+    add $acc7, $acc2
+    adc \$0, $acc3
+
+    mov $acc2, 2*8($r_ptr)
+    mov $acc3, 3*8($r_ptr)
+
+    pop %r12
+    pop %rbx
+    pop %rbp
+    ret
+.size	ll_u256_sqrlo,.-ll_u256_sqrlo
 ___
 }
 
